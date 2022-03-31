907c1789140ab03cc80e92e06d9834f114b74896
klig196 <alialmter502@gmail.com>
2022-03-31 11:59:14 +0400

    lab3

---
+++ lab 3/sty.js
@@ -0,0 +1,6 @@
+
+
+    console.log( y = (0.5 * 0.3 + Math.E* Math.cos(2.9 * 0.3))/(2.9* 0.3-Math.pow(Math.E, -0.3)*Math.sin(2.9*0.3)+1));
+
+    console.log(w = 1.4594740936579165* Math.pow(Math.E,2*0.3)*Math.log(0.5+0.3)-Math.pow(2.9,2* 0.3)*Math.log(2.9-0.3));
+
